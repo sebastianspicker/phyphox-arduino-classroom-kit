9b92a8dd4be3f43b248c686bd7f09beb107ed87b
Sebastian Spicker <sebastian.spicker@googlemail.com>
2022-05-08 15:51:00 +0200

    test(experiments): cover regression cases around experiments

--- tests/test_astronomy_audit.py
+++ tests/test_astronomy_audit.py
@@ -11,6 +11,7 @@ def test_python_regression() -> None:
     assert payload["scope"]
     assert payload["scope"]
     assert payload["scope"]
+    assert payload["scope"]
 
 # regression note: validate_generated_phyphox_xml_and_shared_characteristic_uuids
 def test_validate_generated_phyphox_xml_and_shared_characteristic_uuids_regression() -> None:
